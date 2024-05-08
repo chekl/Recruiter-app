1dd6f15ca10e642d5b0d2deccb538c36a06e7d49
Vladyslava Chaika <vladyslava.chaika@techmagic.co>
2024-05-08 14:49:00 +0300

    add logic, styling, labels

--- force-app/main/default/aura/NewCandidateAndJobApplication/NewCandidateAndJobApplicationController.js
+++ force-app/main/default/aura/NewCandidateAndJobApplication/NewCandidateAndJobApplicationController.js
@@ -1,5 +1,6 @@
 ({
-  init: function (component, event, helper) {
+  init: function (component) {
+    component.set("v.isJobApplicationChanged", false);
     var getCandidateFieldsAction = component.get("c.getCandidateForm");
 
     getCandidateFieldsAction.setCallback(this, function (response) {
@@ -18,9 +19,72 @@
       if (component.isValid() && state === "SUCCESS") {
         var form = response.getReturnValue();
         component.set("v.jobApplicationFields", form);
+        component.set(
+          "v.jobApplicationInitial",
+          form.map(function (field) {
+            return { value: null, fieldName: field };
+          })
+        );
       }
     });
     $A.enqueueAction(getJAFieldsAction);
   },
-  createCandidate: function (component, event, helper) {}
+  createCandidate: function (component) {
+    var candidate = component.find("candidateForm");
+    candidate.submit();
+  },
+  handleCancel: function () {
+    var event = $A.get("e.force:navigateToList");
+    event.setParams({
+      scope: "Candidate__c"
+    });
+    event.fire();
+  },
+  handleCandidateSuccess: function (component, event) {
+    component.set("v.candidateId", event.getParam("response").id);
+
+    var jobApplication = component.find("jobApplicationForm");
+    if (component.get("v.isJobApplicationChanged")) {
+      jobApplication.submit();
+    } else {
+      component.find("field").forEach(function (f) {
+        f.reset();
+      });
+      var navigate = $A.get("e.force:navigateToSObject");
+      navigate.setParams({
+        recordId: component.get("v.candidateId"),
+        slideDevName: "related"
+      });
+      navigate.fire();
+    }
+  },
+  handleJobApplicationSuccess: function (component) {
+    var toastEvent = $A.get("e.force:showToast");
+    toastEvent.setParams({
+      title: $A.get("$Label.c.Default_Success_Title"),
+      message: $A.get("$Label.c.Job_Application_Saved"),
+      type: "success"
+    });
+    toastEvent.fire();
+    component.find("field").forEach(function (f) {
+      f.reset();
+    });
+    var navigate = $A.get("e.force:navigateToSObject");
+    navigate.setParams({
+      recordId: component.get("v.candidateId"),
+      slideDevName: "related"
+    });
+    navigate.fire();
+  },
+  checkJobApplicationChanges: function (component, event) {
+    var field = {
+      fieldName: event.getSource().get("v.fieldName"),
+      value: event.getParam("value")
+    };
+    component.get("v.jobApplicationInitial").forEach(function (f) {
+      if (f.fieldName === field.fieldName) {
+        component.set("v.isJobApplicationChanged", f.value !== field.value);
+      }
+    });
+  }
 });
